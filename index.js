781432cdd90d8a978b14a36d6d958f09a7fa65cc
Maksudr <maksudurrahamanmishu7@gmail.com>
2024-01-22 20:11:49 +0600

    create a token and use jwt token

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT | 5000;
 require('dotenv').config()
 const cors = require('cors');
 
-//iOne = DB_USER
-//Aj8wWbIwwYpRwc5v = DB_PASS
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://evaluation-platform-client.web.app'
+  ],
+  credentials: true
+}))
 app.use(express.json());
+app.use(cookieParser())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.glcj3l3.mongodb.net/?retryWrites=true&w=majority`;
@@ -27,10 +34,27 @@ async function run() {
 
     const reviewCollection = client.db("iOne").collection("reviews");
 
-    app.get("/reviews", async (req, res) => {
-        const result = await reviewCollection.find().toArray();
-        res.send(result);
+
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      console.log(user)
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      console.log(token)
+      res
+        .cookie('token', token,{
+          httpOnly: true,
+          secure: false
+        })
+        .send({success : true})
     })
+
+
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    })
+
+   
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -41,9 +65,9 @@ run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
-    res.send("Company Evaluation Platform is Running")
+  res.send("Company Evaluation Platform is Running")
 })
 
 app.listen(port, () => {
-    console.log(`Company Evaluation Platform is Running on port ${port}`);
+  console.log(`Company Evaluation Platform is Running on port ${port}`);
 })
