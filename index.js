3de75967403c018c64bd105682954fe7ad4a63a5
Maksudr <maksudurrahamanmishu7@gmail.com>
2024-02-04 23:02:04 +0600

    fixt cors essue

--- index.js
+++ index.js
@@ -232,7 +232,7 @@ async function run() {
       return res.send(result);
     })
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   }
   finally {
