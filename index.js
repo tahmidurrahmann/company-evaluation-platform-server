26597b13843baadad79d220dd94727314d15845b
Maksudr <maksudurrahamanmishu7@gmail.com>
2024-02-09 16:29:15 +0600

    change

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(cors({
 
 app.use(cors());
 app.use(express.json());
-
 app.use(cookieParser())
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
